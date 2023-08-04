64a246b2be78f1ef4bc4f1b11f13d3b5a31b8be7
SteveImmanuel <steveandreasimanuel@gmail.com>
2023-08-04 12:46:49 +0900

    Add simple scriptable object

---
+++ Assets/Scripts/GlobalDataScriptableObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GlobalData", menuName = "ScriptableObjects/GlobalData")]
+public class GlobalDataScriptableObject : ScriptableObject
+{
+    public float focalLength;
+    public int width;
+    public int hieght;
+}

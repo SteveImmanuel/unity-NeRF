3d4c5f191f4932be88da5ed3def367b4b55a0077
SteveImmanuel <steveandreasimanuel@gmail.com>
2023-08-08 12:52:13 +0900

    Remove unused data

--- Assets/Scripts/GlobalDataScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GlobalData", menuName = "ScriptableObjects/GlobalData")]
-public class GlobalDataScriptableObject : ScriptableObject
-{
-    public float focalLength;
-    public int width;
-    public int hieght;
-}
